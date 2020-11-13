5b6d261a32ae4552d658109383e4dec8982c83f4
Olivier Van Aken <olivier.va@outlook.com>
2020-11-13 16:32:01 +0100

    added documentation

--- internal/commands/commanders.go
+++ internal/commands/commanders.go
@@ -2,22 +2,31 @@ package commands
 
 import "os/exec"
 
+// Commander interface to run commands.
 type Commander interface {
 	Output(name string, arg ...string) (output string, err error)
 	Run(name string, arg ...string) (err error)
 }
 
+// ExecCommander implementation of the Commander interface.
+// It makes use of exec.Command to run commands.
 type ExecCommander struct{}
 
+// NewExecCommander creates a new ExecCommander.
+// Returns the new ExecCommander.
 func NewExecCommander() *ExecCommander {
 	return &ExecCommander{}
 }
 
+// Output runs a command.
+// Returns the output of the command or an error if it failed.
 func (c ExecCommander) Output(name string, arg ...string) (string, error) {
 	var output, err = exec.Command(name, arg...).Output()
 	return string(output), err
 }
 
+// Output runs a command.
+// Returns an error if it failed.
 func (c ExecCommander) Run(name string, arg ...string) error {
 	return exec.Command(name, arg...).Run()
 }
