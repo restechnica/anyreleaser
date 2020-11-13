ec06d6682e5692159466f939172fbabbb811fbf0
Olivier Van Aken <olivier.va@outlook.com>
2020-11-13 12:38:42 +0100

    added commander interface to wrap exec.Command methods - added git tagger - added create and get tag methods

---
+++ internal/commands/commanders.go
@@ -0,0 +1,19 @@
+package commands
+
+import "os/exec"
+
+type Commander interface {
+	Output(name string, arg ...string) (output string, err error)
+	Run(name string, arg ...string) (err error)
+}
+
+type ExecCommander struct{}
+
+func (c ExecCommander) Output(name string, arg ...string) (string, error) {
+	var output, err = exec.Command(name, arg...).Output()
+	return string(output), err
+}
+
+func (c ExecCommander) Run(name string, arg ...string) error {
+	return exec.Command(name, arg...).Run()
+}
