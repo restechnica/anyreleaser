2a7ad69a97b6070bbf47db784e3d0dbaede98a72
Olivier Van Aken <olivier.va@outlook.com>
2020-11-22 21:18:46 +0100

    added app level config flag - added before action to app to load config before any command

--- cmd/annie.go
+++ cmd/annie.go
@@ -1,19 +1,39 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+
 	"github.com/restechnica/anyreleaser/cmd/get"
 	"github.com/restechnica/anyreleaser/cmd/predict"
 	"github.com/restechnica/anyreleaser/cmd/release"
-	"github.com/urfave/cli/v2"
+)
+
+const (
+	command     = "annie"
+	description = "a CLI which serves as a backbone for your projects"
+)
+
+var (
+	configFlag = &cli.StringFlag{
+		Name:    "config",
+		Aliases: []string{"c"},
+		Usage:   "load configuration from a file",
+		Value:   "./example/.anyreleaser.yaml",
+	}
+
+	flags = []cli.Flag{
+		configFlag,
+	}
 )
 
 // NewApp creates a new CLI app.
 // Returns the cli app.
 func NewApp() (app *cli.App) {
-	var command = "annie"
-	var description = "a CLI which serves as a backbone for your projects"
-
 	app = &cli.App{
+		Before:          before,
+		Flags:           flags,
 		HideHelp:        false,
 		HideHelpCommand: true,
 		Name:            command,
@@ -26,5 +46,11 @@ func NewApp() (app *cli.App) {
 		release.NewCommand(app),
 	}
 
+	return app
+}
+
+func before(c *cli.Context) (err error) {
+	var config = c.String("config")
+	fmt.Println(config)
 	return
 }
